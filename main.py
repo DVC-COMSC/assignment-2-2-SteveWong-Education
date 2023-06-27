7c9feee0ab6ab40b8ae388e36b397836899920aa
SteveWong-Education <136470980+SteveWong-Education@users.noreply.github.com>
2023-06-26 17:01:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def main():
     Use the same variables: celcius fahrenheit 
     ##################################################
     """
-    celcius = input()
+    celcius = float(input())
 
     fahrenheit = float(celcius) * 9/5 + 32
 
-    print(fahrenheit)
+    print(f' Farenheit \t {fahrenheit: 2f}')
     """
     ########################################
     # Do not delete the return statement
